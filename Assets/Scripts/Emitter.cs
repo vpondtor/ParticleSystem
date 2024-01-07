49b4b26f8448181648fa704fa55f529fddccb253
Virak Pond-Tor <virak_pond-tor@brown.edu>
2024-01-06 23:40:52 -0800

    fire!

--- Assets/Scripts/Emitter.cs
+++ Assets/Scripts/Emitter.cs
@@ -27,6 +27,9 @@ public class Emitter : ImmediateModeShapeDrawer {
     private int m_numLiveParticles;
     private readonly Random m_random = new Random();
 
+    // TODO: Refactor this and put it in its own script
+    private readonly Gradient m_gradient = new Gradient();
+
     public void ValidateParameters() {
         totalParticles = Math.Max(totalParticles, 0);
         particlesPerUpdate = Math.Max(particlesPerUpdate, 0);
@@ -37,6 +40,17 @@ public class Emitter : ImmediateModeShapeDrawer {
     }
     
     public void Start() {
+        // TODO: Refactor this and put it in its own script (These are reversed because time1 is actually the birth of a particle)
+        var colors = new GradientColorKey[3];
+        colors[0] = new GradientColorKey(Color.red, 0.5f);
+        colors[1] = new GradientColorKey(Color.yellow, 1.0f);
+
+        var alphas = new GradientAlphaKey[3];
+        alphas[0] = new GradientAlphaKey(0.0f, 0.0f);
+        alphas[1] = new GradientAlphaKey(1.0f, 1.0f);
+
+        m_gradient.SetKeys(colors, alphas);
+        
         m_particlePool = new Particle[totalParticles];
         for (var i = 0; i < m_particlePool.Length; i++) {
             m_particlePool[i] = new Particle() {
@@ -65,6 +79,28 @@ public class Emitter : ImmediateModeShapeDrawer {
         EmitParticles(particlesPerUpdate);
     }
     
+    public void UpdateParticle(int particleId, float deltaSeconds) {
+        // TODO: Refactor this and put it in its own script
+        m_particlePool[particleId].color =
+            m_gradient.Evaluate((float) m_particlePool[particleId].timeToLive / initialTimeToLive);
+        
+        m_particlePool[particleId].position += m_particlePool[particleId].velocity * deltaSeconds;
+        m_particlePool[particleId].timeToLive--;
+    }
+    
+    public void EmitParticles(int numParticles) {
+        var startingIndex = m_numLiveParticles;
+        for (var i = startingIndex; i < startingIndex + numParticles; i++) {
+            if (i >= m_particlePool.Length) {
+                return;
+            }
+            
+            InitializeParticle(i);
+            
+            m_numLiveParticles++;
+        }
+    }
+    
     public void InitializeParticle(int particleId) {
         m_particlePool[particleId].position = transform.position;
         m_particlePool[particleId].velocity = RandomizeDirection(initialDirection).normalized * initialSpeed;
@@ -82,11 +118,6 @@ public class Emitter : ImmediateModeShapeDrawer {
 
         return direction;
     }
-    
-    public void UpdateParticle(int particleId, float deltaSeconds) {
-        m_particlePool[particleId].position += m_particlePool[particleId].velocity * deltaSeconds;
-        m_particlePool[particleId].timeToLive--;
-    }
 
     public void KillParticle(int particleId) {
         var particle = m_particlePool[particleId];
@@ -97,19 +128,6 @@ public class Emitter : ImmediateModeShapeDrawer {
         m_particlePool[particleId] = m_particlePool[m_numLiveParticles - 1];
         m_particlePool[m_numLiveParticles - 1] = particle;
         m_numLiveParticles--;
-    }
-    
-    public void EmitParticles(int numParticles) {
-        var startingIndex = m_numLiveParticles;
-        for (var i = startingIndex; i < startingIndex + numParticles; i++) {
-            if (i >= m_particlePool.Length) {
-                return;
-            }
-            
-            InitializeParticle(i);
-            
-            m_numLiveParticles++;
-        }
     }
     
     public override void DrawShapes(Camera cam){
